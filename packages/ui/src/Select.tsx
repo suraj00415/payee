08760f7e702de044dcabf930f35dc55a72f9f63e
Suraj <suraj00415@gmail.com>
2024-08-28 18:39:28 +0530

    before build

--- packages/ui/src/Select.tsx
+++ packages/ui/src/Select.tsx
@@ -9,7 +9,7 @@ export const Select = ({ options, onSelect }: {
     return (
         <select onChange={(e) => { onSelect(e.target.value) }}
             className="bg-slate-800 border border-slate-700 text-slate-400  rounded-lg w-full p-2.5">
-            {options.map(option => <option value={option.key}>{option.value}</option>)}
+            {options.map((option, index) => <option key={index} value={option.key}>{option.value}</option>)}
         </select>
     )
 }
